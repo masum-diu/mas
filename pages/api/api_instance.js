e015b519f1ababd990ec4658ff5288ecf1f68e59
masum.mollah <masum.mollah@webable.digital>
2025-01-12 14:07:38 +0600

    backendadd

--- pages/api/api_instance.js
+++ pages/api/api_instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://apimas.etherstaging.xyz/api",
+  baseURL: "https://backend.masoutfits.com/api",
 
-  headers: { 
+  headers: {
     "Content-Type": "application/json",
     timeout: 1000,
   },
