a7ecf7a3b99f60b49e90d2908539de3cede8cc75
masum.mollah <masum.mollah@webable.digital>
2025-01-13 12:27:12 +0600

    about

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import Typography from "@mui/material/Typography";
 import { Box, Grid, Stack } from "@mui/material";
-import instance from '../pages/api/api_instance';
+import instance from "../pages/api/api_instance";
 
 const About = () => {
   const [aboutData, setAboutData] = useState(null);
@@ -37,32 +37,27 @@ const About = () => {
   return (
     <Layout>
       <Box sx={{ width: "90%", maxWidth: "1500px", margin: "0 auto" }}>
-        <Grid container spacing={2} py={6}>
+        <Grid container spacing={1} py={2}>
           <Grid item lg={6}>
-           
-              <Typography
-                className="Medium"
-                fontSize={40}
-            
-              >
-                {aboutData?.title}
-              </Typography>
-        
+            <Typography className="Medium" fontSize={40}>
+              {aboutData?.title}
+            </Typography>
           </Grid>
-      
-          </Grid>
-      
-        <Grid container spacing={2} py={6}>
+        </Grid>
+
+        <Grid container spacing={2} py={2}>
           <Grid item lg={5} sx={{ display: "flex", gap: 1, pt: 4 }}>
             <img
-              src={aboutData?.img_path+'/'+aboutData?.feature_image}
+              src={aboutData?.img_path + "/" + aboutData?.feature_image}
               alt="About Image"
               style={{ width: "100%", height: "100%" }}
             />
           </Grid>
           <Grid item lg={7} sx={{ display: "flex", gap: 1, pt: 4 }}>
             <Typography className="Regular" fontSize={16} color={"#bbb"}>
-            <div dangerouslySetInnerHTML={{ __html: aboutData?.description }} />
+              <div
+                dangerouslySetInnerHTML={{ __html: aboutData?.description }}
+              />
             </Typography>
           </Grid>
         </Grid>
