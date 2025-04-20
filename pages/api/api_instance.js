0ba2628e393f6336b4647bdaa65c13544e88d734
masum.mollah <masum.mollah@webable.digital>
2025-04-20 16:02:05 +0600

    retail

--- pages/api/api_instance.js
+++ pages/api/api_instance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
   baseURL: "https://apimas.etherstaging.xyz/api",
-
   headers: {
     "Content-Type": "application/json",
     timeout: 1000,
